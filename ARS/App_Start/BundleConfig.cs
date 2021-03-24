1a6c3dd41c8168c2c74a991daed62821cfe2022f
skyfury2651 <skyfury2651@gmail.com>
2021-03-24 18:18:48 +0700

    push theme

--- ARS/App_Start/BundleConfig.cs
+++ ARS/App_Start/BundleConfig.cs
@@ -25,6 +25,29 @@ namespace ARS
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/custom/css").Include(
+                      "~/Content/custom/bootstrap.min.css",
+                      "~/Content/custom/themify-icons.css",
+                      "~/Content/custom/font-awesome.css",
+                      "~/Content/custom/datepicker.min.css",
+                      "~/Content/custom/fullwidth.css",
+                      "~/Content/custom/animated-on3step.css",
+                      "~/Content/custom/owl.carousel.css",
+                      "~/Content/custom/owl.theme.css",
+                      "~/Content/custom/on3step-style.css",
+                      "~/Content/custom/queries-on3step.css"
+                      ));
+            bundles.Add(new ScriptBundle("~/bundles/custom/js").Include(
+                "~/Scripts/custom/pluginson3step.js",
+                "~/Scripts/custom/bootstrap.min.js",
+                "~/Scripts/custom/bootstrap-datepicker.min.js",
+                "~/Scripts/custom/sticky.js",
+                "~/Scripts/custom/jquery.themepunch.tools.min.js",
+                "~/Scripts/custom/jquery.themepunch.revolution.min.js",
+                "~/Scripts/custom/on3step.js",
+                "~/Scripts/custom/plugin-set.js"
+                        ));
         }
     }
 }
